e1ba89988f2629d5f6112996ef7c54280f9fc200
dotienquangdev <youremail@example.com>
2025-05-27 10:24:39 +0700

    your message

--- validates/products-category.validate.js
+++ validates/products-category.validate.js
@@ -2,7 +2,6 @@ module.exports.createPost = (req, res, next) => {
     if (!req.body.title) {
         req.flash("error", `Vui lòng nhập tiêu đề!`);
         res.redirect("back");
-        // res.redirect("");
         return;
     }
     next();
